7b637e38a97ad88b6731f0f0232378b137b49e23
Christian Perez <perezchristian1012@gmail.com>
2020-01-02 20:29:57 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,31 +1,27 @@
 class SchoolClassesController < ApplicationController
 
-  def index
-    @school_classes = SchoolClass.all
-  end
-
   def new
     @school_class = SchoolClass.new
-  end
-
-  def create
-    @school_class = SchoolClass.new(school_class_params)
-	  @school_class.save
-	  redirect_to school_class_path(@school_class)
   end
 
   def show
     @school_class = SchoolClass.find(params[:id])
   end
 
-  def edit
-    @school_class = SchoolClass.find(params[:id])
+  def create
+    @school_class = SchoolClass.new(school_class_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def update
-    @school_class = SchoolClass.find(params[:id])
-	  @school_class.update(school_class_params(:title , :room_number))
-	  redirect_to school_class_path(@school_class)
+     @school_class = SchoolClass.find(params[:id])
+     @school_class.update(school_class_params)
+     redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+     @school_class = SchoolClass.find(params[:id])
   end
 
   private
