5c591605cebd8d2dea7eb0d9ef38caca82436718
Christian Perez <perezchristian1012@gmail.com>
2020-01-02 20:39:09 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,7 +9,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(school_class_params)
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
@@ -26,8 +26,7 @@ class SchoolClassesController < ApplicationController
 
   private
 
-  def school_class_params
-    params.require(:school_class).permit(:title, :room_number)
+  def class_params(*args)
+     params.require(:school_class).permit(*args)
   end
-
 end
